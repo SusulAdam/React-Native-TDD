8ab2b6d74df5f9ebf5cb3c1ba4ad0f89c9ae0885
SusulAdam <64583871+SusulAdam@users.noreply.github.com>
2022-10-27 19:55:43 +0200

    Write async storage tests which fails

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -2,19 +2,43 @@ import 'react-native';
 import React from 'react';
 import App from '../App';
 import renderer from 'react-test-renderer';
+import AsyncStorage from '@react-native-community/async-storage';
+import {getValue, saveValue} from '../utils/asyncStorage';
 
 it('renders correctly', () => {
   renderer.create(<App />);
 });
 
-describe('getValues', () => {
-  test.todo('if no results exist at key, return an empty array');
+beforeEach(async () => {
+  await AsyncStorage.clear();
+});
 
-  test.todo('returns an array of recent values');
+describe('getValues', () => {
+  test('if no results exist at key, return an empty array', async () => {
+    const result = await getValue();
+    expect(result).toEqual([]);
+  });
+
+  test('returns an array of recent values', async () => {
+    await AsyncStorage.setItem('DEMO::asyncStorage', JSON.stringify([{id: 1}]));
+    const result = await getValue();
+    expect(result).toEqual([{id: 1}]);
+  });
 });
 
 describe('saveValues', () => {
-  test.todo('if no recent values exist, add item');
+  test('if no recent values exist, add item', async () => {
+    await saveValue({id: 1});
 
-  test.todo('add the latest values to the start of the array');
+    const result = await getValue();
+    expect(result).toEqual([{id: 1}]);
+  });
+
+  test('add the latest values to the start of the array', async () => {
+    await saveValue({id: 1});
+    await saveValue({id: 2});
+
+    const result = await getValue();
+    expect(result).toEqual([{id: 2}, {id: 1}]);
+  });
 });
---
+++ utils/asyncStorage.js
@@ -0,0 +1,3 @@
+export const saveValue = async recentValue => {};
+
+export const getValue = async () => {};
