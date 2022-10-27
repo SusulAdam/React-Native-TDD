cb1089828f9186d6daff60816b6028a6f8bc2d19
SusulAdam <64583871+SusulAdam@users.noreply.github.com>
2022-10-27 19:58:13 +0200

    Write the functionality thanks to which the tests will pass

--- utils/asyncStorage.js
+++ utils/asyncStorage.js
@@ -1,3 +1,26 @@
-export const saveValue = async recentValue => {};
+import AsyncStorage from '@react-native-community/async-storage';
 
-export const getValue = async () => {};
+export const saveValue = async recentValue => {
+  try {
+    const existingValue = await getValue();
+    const newRecentValue = [recentValue, ...existingValue];
+    return AsyncStorage.setItem(
+      'DEMO::asyncStorage',
+      JSON.stringify(newRecentValue),
+    );
+  } catch (err) {
+    return;
+  }
+};
+
+export const getValue = async () => {
+  try {
+    const result = await AsyncStorage.getItem('DEMO::asyncStorage');
+    if (result) {
+      return JSON.parse(result);
+    }
+    return [];
+  } catch (err) {
+    return [];
+  }
+};
