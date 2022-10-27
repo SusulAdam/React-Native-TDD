a7898e135e7cc630c3964c2a71327bc7634083c2
SusulAdam <64583871+SusulAdam@users.noreply.github.com>
2022-10-27 19:40:26 +0200

    Clean up test folder and write todo test

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,14 +1,20 @@
-/**
- * @format
- */
-
 import 'react-native';
 import React from 'react';
 import App from '../App';
-
-// Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
   renderer.create(<App />);
+});
+
+describe('getValues', () => {
+  test.todo('if no results exist at key, return an empty array');
+
+  test.todo('returns an array of recent values');
+});
+
+describe('saveValues', () => {
+  test.todo('if no recent values exist, add item');
+
+  test.todo('add the latest values to the start of the array');
 });
